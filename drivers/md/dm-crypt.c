96342066868a06e5c1577c3c30cbb5ee3fc52838
jarpii <efren.saenz@hotmail.com>
2017-01-08 21:58:17 -0500

    Revert "Linux 3.10.42"
    This reverts commit e7d02502b802d7b1be58998abe263e1fd64be788.

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -901,7 +901,7 @@ static void crypt_dec_pending(struct dm_crypt_io *io)
 		return;
 
 	if (io->ctx.req)
-		mempool_free(io->ctx.req, cc->req_pool);
+		crypt_free_req(cc, io->ctx.req, base_bio);
 
 	bio_endio(base_bio, error);
 }
